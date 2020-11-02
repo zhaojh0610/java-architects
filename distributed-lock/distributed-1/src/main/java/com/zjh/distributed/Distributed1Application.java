3abcfd6c29a7620dd79166f3b54aabb4c19cc6f4
zhaojh0610 <zhaojh0610@163.com>
2020-11-02 17:33:55 +0800

    java架构师进阶项目代码

---
+++ distributed-lock/distributed-1/src/main/java/com/zjh/distributed/Distributed1Application.java
@@ -0,0 +1,13 @@
+package com.zjh.distributed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Distributed1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Distributed1Application.class, args);
+    }
+
+}
---
+++ distributed-lock/distributed-1/src/test/java/com/zjh/distributed/Distributed1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjh.distributed;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Distributed1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
