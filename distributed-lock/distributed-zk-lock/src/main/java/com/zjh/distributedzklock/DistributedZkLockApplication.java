3c38cd916c526f519adfe889c4ab89eebe6dcba2
zhaojh0610 <zhaojh0610@163.com>
2020-11-04 12:21:10 +0800

    code -des 用curator依赖zookeeper实现分布式锁

--- distributed-lock/distributed-zk-lock/src/main/java/com/zjh/distributedzklock/DistributedZkLockApplication.java
+++ distributed-lock/distributed-zk-lock/src/main/java/com/zjh/distributedzklock/DistributedZkLockApplication.java
@@ -1,8 +1,16 @@
 package com.zjh.distributedzklock;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * @author zhaojh
+ */
 @SpringBootApplication
 public class DistributedZkLockApplication {
 
@@ -10,4 +18,10 @@ public class DistributedZkLockApplication {
         SpringApplication.run(DistributedZkLockApplication.class, args);
     }
 
+    @Bean(initMethod = "start",destroyMethod = "close")
+    public CuratorFramework getCuratorFramework() {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("192.168.120.131:2181", retryPolicy);
+        return client;
+    }
 }
--- distributed-lock/distributed-zk-lock/src/main/java/com/zjh/distributedzklock/controller/ZookeeperController.java
+++ distributed-lock/distributed-zk-lock/src/main/java/com/zjh/distributedzklock/controller/ZookeeperController.java
@@ -2,10 +2,14 @@ package com.zjh.distributedzklock.controller;
 
 import com.zjh.distributedzklock.lock.ZkLock;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zhaojh
@@ -15,9 +19,12 @@ import java.io.IOException;
 @Slf4j
 public class ZookeeperController {
 
+    @Autowired
+    private CuratorFramework client;
+
     @RequestMapping("zooLock")
     public String zooLock() {
-        try (ZkLock zkLock = new ZkLock()){
+        try (ZkLock zkLock = new ZkLock()) {
             if (zkLock.getLock("order")) {
                 log.info("我进入了锁。。。");
             }
@@ -27,5 +34,31 @@ public class ZookeeperController {
             exception.printStackTrace();
         }
         return "执行完毕。。。";
+    }
+
+    @RequestMapping("curatorLock")
+    public String curatorLock() {
+        log.info("我进入了方法。。。");
+        InterProcessMutex lock = new InterProcessMutex(client, "/curator");
+        try {
+            if (lock.acquire(30, TimeUnit.SECONDS)) {
+                log.info("我进入了锁。。。");
+
+                Thread.sleep(10000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            log.info("我释放了锁。。。");
+            try {
+                lock.release();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+        log.info("执行完毕了。。。");
+        return "我执行完毕了。。。";
     }
 }
