3c4e3c0987fc81168fa1626f5fa4b33adbef5e59
zhaojh0610 <zhaojh0610@163.com>
2020-11-09 23:11:03 +0800

    zjh -d 实现分布式事务-基于MQ配置生产者和消费者

---
+++ sharding-sphere-demo/tcc-demo/src/main/java/com/zjh/tccdemo/config/RocketmqConfig.java
@@ -0,0 +1,33 @@
+package com.zjh.tccdemo.config;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhaojh
+ * @date 2020/11/9 22:59
+ */
+@Configuration
+public class RocketmqConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "shutdown")
+    public DefaultMQProducer producer() {
+        DefaultMQProducer producer = new DefaultMQProducer("paymentGroup");
+        producer.setNamesrvAddr("localhost:9876");
+        return producer;
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "shutdown")
+    public DefaultMQPushConsumer pushConsumer(@Qualifier("messageListener") MessageListenerConcurrently messageListener) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("paymentGroup");
+        consumer.setNamesrvAddr("localhost:9876");
+        consumer.subscribe("payment", "*");
+        consumer.registerMessageListener(messageListener);
+        return consumer;
+    }
+}
